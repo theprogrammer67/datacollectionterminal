a230508f67da6a038e739a3822e461dbf7e19df2
Стовпец Игорь <stoi@rarus.ru>
2020-12-10 15:48:47 +0300

    Improve: Вынести хендлеры сервера в отдельный класс

--- app/src/main/java/ru/rarus/datacollectionterminal/RestServer.kt
+++ app/src/main/java/ru/rarus/datacollectionterminal/RestServer.kt
@@ -10,24 +10,22 @@ import java.net.InetSocketAddress
 import java.net.URI
 import java.util.*
 import java.util.concurrent.Executors
+import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.full.memberProperties
 
 class RestServer {
     private var mHttpServer: HttpServer? = null
     private var _started = false
     val started: Boolean get() = (mHttpServer != null) && (_started)
     private val gson = Gson()
+    private val handlers = Handlers(this)
 
     fun start(port: Int) {
         try {
             mHttpServer = HttpServer.create(InetSocketAddress(port), 0)
             mHttpServer!!.executor = Executors.newCachedThreadPool()
-
-            mHttpServer!!.createContext("/", rootHandler)
-            mHttpServer!!.createContext("/index", rootHandler)
-            mHttpServer!!.createContext("/test", testHandler)
-
+            addHandlers()
             mHttpServer!!.start()
-
             _started = true
         } catch (e: IOException) {
             e.printStackTrace()
@@ -47,7 +45,7 @@ class RestServer {
         return if (s.hasNext()) s.next() else ""
     }
 
-    private fun sendResponse(httpExchange: HttpExchange, responseText: String) {
+    fun sendResponse(httpExchange: HttpExchange, responseText: String) {
         val rawBody = responseText.toByteArray(Charsets.UTF_8)
         httpExchange.responseHeaders.add("Content-Type", "text/html")
         httpExchange.responseHeaders.add("charset", "utf-8")
@@ -58,7 +56,7 @@ class RestServer {
         os.close()
     }
 
-    private fun <T> sendResponse(httpExchange: HttpExchange, obj: T) {
+    fun <T> sendResponse(httpExchange: HttpExchange, obj: T) {
         val jsonObj = gson.toJson(obj)
         val rawBody = jsonObj.toByteArray(Charsets.UTF_8)
         httpExchange.responseHeaders.add("Content-Type", "text/JSON")
@@ -69,40 +67,56 @@ class RestServer {
         os.close()
     }
 
-    private fun URI.getFileName(): String {
-        var _path = this.path
-        while (_path.startsWith("/")) _path = _path.substring(1)
-        while (_path.endsWith("/")) _path = _path.substring(0, _path.length - 1);
-
-        val segments: List<String> = _path.split("/")
-        return if (segments.size > 1)
-            segments[1]
-        else
-            ""
+    private fun addHandlers() {
+        val handlerProps =
+            Handlers::class.memberProperties.filter { it.findAnnotation<RequestHandler>() != null }
+        handlerProps.forEach { handler ->
+            mHttpServer!!.createContext(
+                handler.findAnnotation<RequestHandler>()!!.path,
+                handler.get(handlers) as HttpHandler
+            )
+        }
     }
+}
+
+fun URI.getFileName(): String {
+    var _path = this.path
+    while (_path.startsWith("/")) _path = _path.substring(1)
+    while (_path.endsWith("/")) _path = _path.substring(0, _path.length - 1)
+
+    val segments: List<String> = _path.split("/")
+    return if (segments.size > 1)
+        segments[1]
+    else
+        ""
+}
 
 //---------- Endpoint handlers----------
+class Handlers(private val server: RestServer) {
 
-    // Handler for root endpoint
-    private val rootHandler = HttpHandler { exchange ->
+    // root endpoint
+    @RequestHandler("/")
+    val rootHandler = HttpHandler { exchange ->
         when (exchange!!.requestMethod) {
             "GET" -> {
                 val htmlResponse =
                     "<html><head><meta http-equiv=\"content-type\" content=\"text/html; charset=utf-8\" /></head>" +
                             "<body>Добро пожаловать!</body></html>"
-                sendResponse(exchange, htmlResponse)
+                server.sendResponse(exchange, htmlResponse)
             }
         }
     }
 
-    // Handler for test endpoint
-    private val testHandler = HttpHandler { exchange ->
+    // test endpoint
+    @RequestHandler("/test")
+    val testHandler = HttpHandler { exchange ->
         when (exchange!!.requestMethod) {
             "GET" -> {
                 val data = TestData(exchange.requestURI.getFileName(), 123, "Test123")
-                sendResponse<TestData>(exchange, data)
+                server.sendResponse<TestData>(exchange, data)
             }
         }
     }
-
 }
+
+annotation class RequestHandler(val path: String)
