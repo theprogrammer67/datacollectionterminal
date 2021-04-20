5b082aea4399a0b34549974b1066f9190ab9038f
Стовпец Игорь <stoi@rarus.ru>
2021-04-20 23:13:52 +0300

    add: ip adreess value on settings

--- app/src/main/java/ru/rarus/datacollectionterminal/ui/SettingsActivity.kt
+++ app/src/main/java/ru/rarus/datacollectionterminal/ui/SettingsActivity.kt
@@ -1,15 +1,25 @@
 package ru.rarus.datacollectionterminal.ui
 
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.text.format.Formatter
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import ru.rarus.datacollectionterminal.R
 
+
 class SettingsActivity : AppCompatActivity() {
+
+    private fun getIpAddress(): String {
+        val wm = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+        return Formatter.formatIpAddress(wm.connectionInfo.ipAddress)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings_activity)
+        setContentView(R.layout.activity_settings)
+
         if (savedInstanceState == null) {
             supportFragmentManager
                 .beginTransaction()
@@ -17,6 +27,9 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val tvIpAddress = findViewById<TextView>(R.id.tvIpAddress)
+        tvIpAddress.text = getIpAddress()
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
